56ad12a8fc6c4ca771a25bd2d0ef88597c76a181
poppy1006 <poppyinfo0@gmail.com>
2026-01-08 15:33:53 +0530

    fix: :sparkles: cahnged from firebase to supabase !

--- lib/authentication/auth_service.dart
+++ lib/authentication/auth_service.dart
@@ -1,55 +1,55 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
+// class AuthService {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  /// REGISTER USER
-  Future<String?> registerUser({
-    required String name,
-    required String email,
-    required String password,
-    String role = 'customer',
-  }) async {
-    try {
-      final cred = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+//   /// REGISTER USER
+//   Future<String?> registerUser({
+//     required String name,
+//     required String email,
+//     required String password,
+//     String role = 'customer',
+//   }) async {
+//     try {
+//       final cred = await _auth.createUserWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
 
-      await _db.collection('users').doc(cred.user!.uid).set({
-        'name': name,
-        'email': email,
-        'role': role,
-        'approved': role == 'parking_admin' ? false : true,
-        'createdAt': FieldValue.serverTimestamp(),
-      });
+//       await _db.collection('users').doc(cred.user!.uid).set({
+//         'name': name,
+//         'email': email,
+//         'role': role,
+//         'approved': role == 'parking_admin' ? false : true,
+//         'createdAt': FieldValue.serverTimestamp(),
+//       });
 
-      return null;
-    } catch (e) {
-      return e.toString();
-    }
-  }
+//       return null;
+//     } catch (e) {
+//       return e.toString();
+//     }
+//   }
 
-  /// LOGIN USER
-  Future<String?> loginUser({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return null;
-    } catch (e) {
-      return e.toString();
-    }
-  }
+//   /// LOGIN USER
+//   Future<String?> loginUser({
+//     required String email,
+//     required String password,
+//   }) async {
+//     try {
+//       await _auth.signInWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
+//       return null;
+//     } catch (e) {
+//       return e.toString();
+//     }
+//   }
 
-  /// LOGOUT
-  Future<void> logout() async {
-    await _auth.signOut();
-  }
-}
+//   /// LOGOUT
+//   Future<void> logout() async {
+//     await _auth.signOut();
+//   }
+// }
