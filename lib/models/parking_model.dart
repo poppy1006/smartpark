6a20ad48f14acf650d2085d4606e37953c39a563
poppy1006 <poppyinfo0@gmail.com>
2026-01-20 10:53:15 +0530

    Updated Parking Model

--- lib/models/parking_model.dart
+++ lib/models/parking_model.dart
@@ -1,3 +1,33 @@
+// import 'package:latlong2/latlong.dart';
+
+// class ParkingModel {
+//   final String id;
+//   final String name;
+//   final double latitude;
+//   final double longitude;
+
+//   ParkingModel({
+//     required this.id,
+//     required this.name,
+//     required this.latitude,
+//     required this.longitude,
+//   });
+
+//   /// Convert Supabase row → Dart object
+//   factory ParkingModel.fromMap(Map<String, dynamic> map) {
+//     return ParkingModel(
+//       id: map['id'],
+//       name: map['name'],
+//       latitude: (map['latitude'] as num).toDouble(),
+//       longitude: (map['longitude'] as num).toDouble(),
+//     );
+//   }
+
+//   /// Helper for map usage
+//   LatLng get latLng => LatLng(latitude, longitude);
+// }
+
+
 import 'package:latlong2/latlong.dart';
 
 class ParkingModel {
@@ -6,23 +36,34 @@ class ParkingModel {
   final double latitude;
   final double longitude;
 
+  final int totalSlots;
+  final int availableSlots;
+  final double hourlyPrice;
+
   ParkingModel({
     required this.id,
     required this.name,
     required this.latitude,
     required this.longitude,
+    required this.totalSlots,
+    required this.availableSlots,
+    required this.hourlyPrice,
   });
 
-  /// Convert Supabase row → Dart object
   factory ParkingModel.fromMap(Map<String, dynamic> map) {
     return ParkingModel(
-      id: map['id'],
-      name: map['name'],
+      id: map['id'].toString(),
+      name: map['name'] ?? '',
       latitude: (map['latitude'] as num).toDouble(),
       longitude: (map['longitude'] as num).toDouble(),
+      totalSlots: map['total_slots'] ?? 0,
+      availableSlots: map['available_slots'] ?? 0,
+      hourlyPrice:
+          map['hourly_price'] == null
+              ? 0
+              : (map['hourly_price'] as num).toDouble(),
     );
   }
 
-  /// Helper for map usage
   LatLng get latLng => LatLng(latitude, longitude);
 }
